c739fa46fad668d70eb40c861832ed6f27fc8377
TimWeyel <tweyel@gmail.com>
2022-08-29 12:32:47 -0700

    add navigation links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to article_path
   end
 
 end
