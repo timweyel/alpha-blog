733977834dc6c10b7c78ade1a0e8096d62ef7abc
TimWeyel <tweyel@gmail.com>
2022-11-18 15:27:34 -0800

    add user id to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  belongs_to :user
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
                       length: { minimum: 3, maximum: 25 }
